e907f09d17b50a9b5dbb77d48dfda04ef06b9466
flottant <53401195+flottant@users.noreply.github.com>
2020-10-06 09:29:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: '凛空の小屋 服务器状态监控',
+  SiteName: 'flottantの小屋 服务器状态监控',
 
   // 站点链接
   SiteUrl: '/',
@@ -10,11 +10,11 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'ur1004328-7deb9fc4df464fb1d9674772',
+    'ur986239-2391e7f367c6dd2b3ba56f3c',
   ],
 
   // 是否显示监测站点的链接
-  ShowLink: false,
+  ShowLink: true,
 
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
@@ -24,12 +24,8 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: '晓空blog',
-      url: 'https://blog.moeworld.tech/'
-    },
-    {
-      text: '关于',
-      url: 'http://about.rcraft.ml'
+      text: '首页',
+      url: 'https://flot.tech'
     }
   ]
 };
